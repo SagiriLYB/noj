12f968509e654c3ff8cfc0a33da7390f8869b60a
SagiriLYB <13984175703@163.com>
2021-10-13 18:15:32 +0800

    homework

--- 022.cpp
+++ 022.cpp
@@ -1,15 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int Judge(int n1,int n2)
+int Judge(int num[10])
 {
-    int n,t1,t2;int num[10];
-    n1*=100000;
-    n=n1+n2;
-    for(t1=10,t2=1;t1<=100000000;t1*=10,t2++)
+    int t1,t2;int num[10];
+    for(t1=1;t1<=9;t1++)
     {
-        num[t2]=n%t1;
-        cout<<num[t2]<<endl;
+        for(t2=t1+1;t2<=9;t2++)
+        {
+            if(num[t1]==num[t2]) return 0;
+        }
     }
-    return 0;
+    return 1;
+}
+
+int Input(int n1,int n2)
+{
+    
+}
+
+int main()
+{
+    int n1,n2,n;int t;
+    cin>>n;
+    for(n1=98765;n1>=12345;n1--)
+    {
+        for(n2=12345;n2<=98765;n2++)
+        {
+            if(n2/n1==n)
+            {
+            }
+        }
+    }
 }
--- test.cpp
+++ test.cpp
@@ -2,21 +2,4 @@
 #include <math.h>
 using namespace std;
 
-void Judge(int n1,int n2)
-{
-    int n,t1,t2;int num[10];
-    n1*=100000;
-    n=n1+n2;
-    for(t1=10,t2=1;t1<=100000000;t1*=10,t2++)
-    {
-        num[t2]=n%t1;
-        cout<<num[t2]<<endl;
-    }    
-}
-
-int main()
-{
-    int n1=12345,n2=67890;
-    printf("Judge(n1,n2)");
-    return 0;
-}
+int 
