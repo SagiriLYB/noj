9fb5bf665c428cf1e73dd7ca0bc73f6db32db3bf
SagiriLYB <13984175703@163.com>
2021-10-14 19:00:25 +0800

    homework

--- 022.cpp
+++ 022.cpp
@@ -1,22 +1,48 @@
 #include <iostream>
 using namespace std;
 
-int Judge(int num[10])
+int num[10];
+
+int Judge(int num[10])//判断是否有相等数
 {
-    int t1,t2;int num[10];
+    int t1,t2;
     for(t1=1;t1<=9;t1++)
     {
         for(t2=t1+1;t2<=9;t2++)
         {
             if(num[t1]==num[t2]) return 0;
+            else return 1;
         }
     }
-    return 1;
 }
 
-int Input(int n1,int n2)
+void Input(int n1,int n2)//将数字转化数组
 {
-    
+    int t;
+    for(t=4;t>=0;t--)
+    {
+        num[t]=n1%10;
+        n1/=10;
+    }
+    for(t=5;t<10;t++)
+    {
+        num[t]=n2%10;
+        n2/=10;
+    }
+}
+
+void Output(int num[10])//输出
+{
+    int t;
+    for(t=0;t<5;t++)
+    {
+        printf("%d",num[t]);
+    }
+    printf("/");
+    for(t=5;t<10;t++)
+    {
+        printf("%d",num[t]);
+    }
 }
 
 int main()
--- test.cpp
+++ test.cpp
@@ -1,5 +1,39 @@
 #include <iostream>
-#include <math.h>
 using namespace std;
 
-int 
+int num[10];int n1=1234567790;int t;
+
+int main()
+{
+    int t1,t2=10;
+    for(t1=9;t1>=0;t1--)
+    {
+        num[t1]=n1%t2;
+        n1/=10;
+        cout<<n1<<"-"<<num[t1]<<endl;
+    }
+    for(t=0;t<5;t++)
+    {
+        printf("%d",num[t]);
+    }
+    printf("/");
+    for(t=5;t<10;t++)
+    {
+        printf("%d",num[t]);
+    }
+    printf("\n");
+    for(t1=1;t1<=9;t1++)
+    {
+        for(t2=t1+1;t2<=9;t2++)
+        {
+            if(num[t1]==num[t2])
+            {
+                cout<<1<<endl;
+                return 0;
+            }
+        }
+    }
+    cout<<0<<endl;
+    getchar();
+    return 0;
+}
