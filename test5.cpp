bd1f3c8cff34c6b493e0556794889b2142a7766c
SagiriLYB <13984175703@163.com>
2021-10-22 15:52:56 +0800

    2021/10/22;15:52

--- test5.cpp
+++ test5.cpp
@@ -2,7 +2,12 @@
 
 int main()
 {
-    int a=10;int b=3;
-    std::cout<<a/b<<std::endl;
+    int i=10,sum=0;
+    for(int i=0;i!=10;++i)
+    {
+        sum+=i;
+        std::cout<<i<<" ";
+    }
+    std::cout<<i<<" "<<sum<<std::endl;
     return 0;
 }
