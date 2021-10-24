5bbe4c1b7e1b0eb179548e4b8e6b85e1619baf80
SagiriLYB <13984175703@163.com>
2021-10-24 22:44:22 +0800

    2021/10/24;22:44

--- test.cpp
+++ test.cpp
@@ -1,39 +1,13 @@
-#include <iostream>
+#include<iostream>
+#include<stdio.h>
 using namespace std;
-
-int num[10];int n1=1234567790;int t;
 
 int main()
 {
-    int t1,t2=10;
-    for(t1=9;t1>=0;t1--)
-    {
-        num[t1]=n1%t2;
-        n1/=10;
-        cout<<n1<<"-"<<num[t1]<<endl;
-    }
-    for(t=0;t<5;t++)
-    {
-        printf("%d",num[t]);
-    }
-    printf("/");
-    for(t=5;t<10;t++)
-    {
-        printf("%d",num[t]);
-    }
-    printf("\n");
-    for(t1=1;t1<=9;t1++)
-    {
-        for(t2=t1+1;t2<=9;t2++)
-        {
-            if(num[t1]==num[t2])
-            {
-                cout<<1<<endl;
-                return 0;
-            }
-        }
-    }
-    cout<<0<<endl;
-    getchar();
+    double a,b,c,x,z;
+    scanf("%lf%lf%lf",&a,&b,&c);
+    x=a+b+c;
+    z=(a+b+c)/3;
+    printf("%lf\n%lf\n",x,z);
     return 0;
 }
