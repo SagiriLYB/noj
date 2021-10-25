c41f282aff32cc48a56c60170ec1e80cd933848f
SagiriLYB <13984175703@163.com>
2021-10-25 16:54:00 +0800

    2021/10/25;16:53

--- 024.cpp
+++ 024.cpp
@@ -2,25 +2,30 @@
 #include <cstdio>
 #include <cmath>
 
+using std::cout;
+using std::cin;
+using std::endl;
+
 int a,b,l;
 int n,num[100];
 int i,temp=0;
 
 void Output()//输出函数
 {
-    std::cout<<n<<'.';
-    for(i=0;i<l;i++) std::cout<<num[i];
+    cout<<n<<'.';
+    for(i=0;i<l;i++) cout<<num[i];
 }
 
 int main()
 {
-    std::cin>>a>>b>>l;
+    cin>>a>>b>>l;
     n=a/b;//整数部分计算
+    temp=a%b;
     for(i=0;i<l;i++)//小数部分计算
     {
-        temp=a%b;
         num[i]=10*temp/b;temp=10*temp%b;
     }
-    Output;
+    if(num[l-1]>=5) num[l-1]+=1;//四舍五入
+    Output();
     return 0;
 }
--- test2.cpp
+++ test2.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
-using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
 
-int num[110];
+int a,b,l;
+int n,num[100];
+int i,temp=0;
 
-void Cal(int a,int b)
+int main()
 {
-    int c;
-    c=a/b;
-    
+    cin>>a>>b>>l;
+    n=a/b;//整数部分计算
+    cout<<"n="<<n<<endl;
+    temp=a%b;
+    for(i=0;i<l;i++)//小数部分计算
+    {
+        num[i]=10*temp/b;temp=10*temp%b;
+        cout<<"temp="<<temp<<endl;
+    }
+    if(num[l-1]>=5) num[l-1]+=1;
+    for(i=0;i<l;i++) cout<<num[i]<<endl;
+    return 0;
 }
