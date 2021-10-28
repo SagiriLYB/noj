de2f8711ad75a4429de4640615ff77d57721c314
SagiriLYB <13984175703@163.com>
2021-10-28 22:51:46 +0800

    2021/10/28;22:51

--- test4.cpp
+++ test4.cpp
@@ -1,16 +1,40 @@
 #include <iostream>
+#include <ctime>
+using namespace std;
 
- int i,a=1,b=6,l=4,n=12,num[100]={1,2,3,4,5};
-
-void Output()//输出函数
+void QuickSort(int A[],int n,int left,int right)
 {
-    std::cout<<n<<'.';
-    for(i=0;i<l;i++) std::cout<<num[i];
+    int i,j,t;
+    if(left<right)
+    {
+        i=left;j=right+1;
+        while(1)
+        {
+            while(i+1<n&&A[++i]<A[left]);
+            while(j-1>-1&&A[--j]>A[left]);
+            if(i>=j) break;
+            t=A[i],A[i]=A[j],A[j]=t;
+        }
+        t=A[left],A[left]=A[j],A[j]=t;
+        QuickSort(A,n,left,j-1);
+        QuickSort(A,n,j+1,right);
+    }
 }
+
+#define N 10
 
 int main()
 {
-    Output();
+    int A[N],i;
+    srand((unsigned int)time(0));
+    for(i=0;i<N;i++)
+    {
+        A[i]=rand()%100;
+        cout<<A[i]<<" ";
+    }
+    cout<<endl;
+    QuickSort(A,N,0,N-1);
+    for(i=0;i<N;i++) cout<<A[i]<<" ";
     return 0;
-}
 
+}
