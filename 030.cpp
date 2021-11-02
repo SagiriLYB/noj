b0ebe7e5b7039f3af04a4489911520979ffc1756
SagiriLYB <13984175703@163.com>
2021-11-02 11:55:47 +0800

    2021/11/2;11:55

--- 030.cpp
+++ 030.cpp
@@ -4,20 +4,40 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+int N[10];
+int i1,i2,i3;
+int n;
+int temp;
+
+void In(int n)//转换为数组
+{
+    for(i1=0;n>0;i1++)
+    {
+        N[i1]=n%10;
+        n/=10;
+    }
+    temp=i1;
+}
+
+int Cal()//统计1数量
+{
+    int t=0;
+    for(i2=0;i2<temp;i2++)
+    {
+        if(N[i2]==1) t++;
+    }
+    return t;
+}
+
 int main()
 {
-    int i1,i2,n,t=0,add=1;
     cin>>n;
-    for(i2=10;i2<=n;i2*=10)
+    int sum=0;
+    for(i3=1;i3<=n;i3++)
     {
-        for(i1=1;i1<=n;i1++)
-        {
-            cout<<"i1i2="<<i1<<' '<<i2<<endl;
-            if(i1%i2==1) t+=add;
-            if(i1%i2==0) t++;
-            if(i1>=i2) add++;
-        }
+        In(i3);
+        sum=sum+Cal();
     }
-    cout<<t;
+    cout<<sum;
     return 0;
 }
