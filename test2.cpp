c4e76c5a09c7e032f4a8472be52a63db17b6063f
SagiriLYB <13984175703@163.com>
2021-11-09 17:26:18 +0800

    2021/11/9;17:26

--- test2.cpp
+++ test2.cpp
@@ -1,33 +1,11 @@
 #include <iostream>
-
-using std::cout;
-using std::cin;
-using std::endl;
-
-int Judge1(int n)//判断是否存在素数
-{
-    int i;
-    for(i=2;i<n;++i)
-    {
-        if(n%i==0) return 0;//为合数
-    }
-    return 1;//为素数
-}
-
-int Judge2(int n)//判断是否为合数世纪
-{
-    int i;
-    for(i=100*n;i<100*(n+1);i++)
-    {
-        if(Judge1(i)==1) return 0;//不为
-    }
-    return 1;//为
-}
-
+#include <string>
+using namespace std;
 int main()
-{
-    int i;
-    cout<<Judge2(16718)<<endl;
-    cout<<Judge2(16719)<<endl;
-    return 0;
+{	
+	int a=0;
+	cin>>a;
+	if(a<10) cout<<'1';
+	else if(a<20) cout<<'2';
+	return 0;
 }
--- test3.cpp
+++ test3.cpp
@@ -4,3 +4,17 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+int main()
+{
+    int n,sum;
+    int i1,i2,i3;
+    for(i1=1;i1<=n;i1++)
+    {
+        for(i2=10;i2<=n;i2*=10)
+        {
+            if(n%i2==1) sum++;
+            if(n/i2==1) sum++;
+        }
+    }
+    return 0;
+}
