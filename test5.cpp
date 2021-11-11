fa1f0aa83afa36ebd252fd6cf222fba3ece848e3
SagiriLYB <13984175703@163.com>
2021-11-11 17:16:36 +0800

    2021.11.11；17:16

--- test5.cpp
+++ test5.cpp
@@ -1,26 +1,19 @@
+#include <string>
 #include <iostream>
 
-using namespace std;
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
 
 int main()
 {
-    int a,c=0,d=0,e,n,b;
-    cin>>e;
-    for(n=0;d<e;n++)
+    int i;
+    string ms1,ms2;
+    while(cin>>ms1)
     {
-        for(a=1;a<=99;a++)
-        {
-            for(b=2;b<(a+n*100);b++)
-            {
-                if((a+n*100)%b==0)
-                {
-                    c++;break;
-                }
-            }
-        if(c==99){d++;break;}
-        if(a==99)c=0;
-        }
+        ms2=ms1+" "+ms2;
     }
-    printf("%d %d",n*100,n*100+99);
+    cout<<ms2<<endl;
     return 0;
 }
--- test6.cpp
+++ test6.cpp
@@ -1,23 +1,19 @@
-#include <iostream>
 #include <string>
-#include <vector>
-using namespace std;
-void main()
-{	
-	vector<string> My_vector;
-	string istring;
-	while (cin >> istring)
-	{
-		My_vector.push_back(istring);
-	}
-	for (int i = 0;i < My_vector.size();i++)
-		for (int j = 0;j < My_vector[i].length();j++)
-		{
-			My_vector[i][j] = toupper(My_vector[i][j]);
-		}
-	for (int i = 0;i < My_vector.size();i++)
-	{
-		cout<<My_vector[i]<<endl;
-	}
-}
+#include <iostream>
 
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    int i;
+    string ms1,ms2;
+    while(cin>>ms1)
+    {
+        ms2=ms1+ms2+" ";
+    }
+    cout<<ms2<<endl;
+    return 0;
+}
