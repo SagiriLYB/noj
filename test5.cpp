d2abec8d20c7ae7c03c8f91a434f2799336c261f
SagiriLYB <13984175703@163.com>
2021-12-18 19:03:42 +0800

    2021/12/18;19:03

--- test5.cpp
+++ test5.cpp
@@ -1,26 +1,43 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+using std::stringstream;
+
 #include<iostream>
 #include<iomanip>
 #include<stdio.h>
 #include<math.h>
 using namespace std;
 
-class MyClass
-
-
-{
-
-
-public:
-
-
-MyClass(){cout<<'1';}
-
-
-};
-
 int main()
 {
-    MyClass a,b[2],*p[2];
+	int m,n,s1=1,s2=1,i,k,c;
+	cin>>m>>n;
+	k=m-n+1;
+	if(m>=n)
+    {
+        for(i=1;i<=n;i++)
+    {
+        s1*=k;
+        k++;
+    }
+    for(i=1;i<=n;i++)
+    {
+        s2*=n;
+        n--;
+    }
+    c=s1/s2;
+    cout<<c;
+    }
+	else
+    cout<<"wrong";
     return 0;
 
 }
