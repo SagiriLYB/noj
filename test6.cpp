b3f617d1b717148e59a4395a0b59ac36d7aafc6f
SagiriLYB <13984175703@163.com>
2021-12-20 23:16:20 +0800

    2021/12/20;23:16

--- test6.cpp
+++ test6.cpp
@@ -1,44 +1,23 @@
-#include<iostream>
-#include<stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include<iomanip>
-#include <string.h>
-using namespace std;
+#include <string>
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+using std::stringstream;
+
+struct Sales_data
+{
+	std::string bookNo;
+	unsigned units_sold=0;
+	double revenue=0.0;
+};
 
 int main()
 {
-    char A[100000];
-    int n,i=0,a=0,b=0,j;
-    double x;
-    while(1)
-	{
-		scanf("%c",&A[i]);
-		if(A[i]=='\n')
-			break;
-		i++;
-	}
-	for(j=0;j<i;j++)
-	{
-		if(A[j]=='S')
-		{
-			printf("WA");
-			goto OH;
-		}
-		if(A[j]=='U')
-			a++;
-		if(A[j]=='D')
-			b++;
-	}
-	x=a*1.0/(a+b)*1.0;
-	if(x-0.5>0.003||x-0.5<-0.003)
-		{printf("Fail");
-    goto OH;}
-	if(x-0.5<=0.003||x-0.5>=-0.003)
-        {
-            if(a=b)
-        cout<<1<<"/"<<2;
-	}
-
-	OH:return 0;
+	
 }
