def8b8f84f9b1a2af4df6408cd5558742ac44972
SagiriLYB <92093235+SagiriLYB@users.noreply.github.com>
2022-01-12 21:17:50 +0800

    2022/1/12;21:17

--- test4.cpp
+++ test4.cpp
@@ -2,64 +2,25 @@
 #include <iostream>
 #include <vector>
 #include <sstream>
+#include <fstream>
+#include <iomanip>
 
-using std::cin;
-using std::cout;
-using std::endl;
-using std::string;
-using std::vector;
-using std::stringstream;
+using namespace std;
 
-
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 int main()
 {
-    double A[100];
-    int i=0,j=0,k=0,l=0,sum=0;
-    while(1)
+    ifstream infile("2.txt",ios::in);
+    ofstream outfile("21.txt",ios::out);
+    string line;
+    int i=1;
+    while(getline(infile,line))
 	{
-		scanf("%lf",&A[i]);
-		if(A[i]==0)
-			break;
-		i++;
-	}
-    for(j=0;j<i;j++)
-	{
-		if(A[j]==2)printf("1\n");
-		else{
-		int B[10000],m=1,number=0;
-		double n;
-		B[0]=2;
-		for(k=3;k<=A[j];k+=2)
+		for(int i=0;i<10;i++)
 		{
-			n=(double)k;
-			for(l=2;l<=sqrt(n);l++)
-			{
-				if(k%l==0)
-					goto ai;
-			}
-			B[m]=k;
-			m++;
-			ai:;
+			outfile<<line[i];
 		}
-		for(k=0;k<m;k++)
-		{
-			sum=0;
-			for(l=k;l<m;l++)
-			{
-				sum+=B[l];
-				if(sum==A[j])
-				{
-					number++;
-					break;
-				}
-			}
-		}
-		printf("%d\n",number);
-		}
+		outfile<<endl;
 	}
-	return 0;
+
+    return 0;
 }
